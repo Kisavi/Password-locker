80799f04a1f26abdf26988c740df3fe9eeab560b
Kisavi <deniskisavi@gmail.com>
2022-04-22 22:50:44 +0300

    create a test to check if our user object is being saved in our user_list

--- user-test.py
+++ user-test.py
@@ -1,12 +1,13 @@
 import unittest
 
-from user import User #import  user class
+from user import User  # import  user class
 
 
 class TestUser(unittest.TestCase):
     """
     TestUser class defines test cases for the user  class behaviours
     """
+
     def setUp(self):
         """
         this method allows us to define instructions that will execute before each test case.
@@ -15,11 +16,14 @@ class TestUser(unittest.TestCase):
         self.new_user = User("James", "w3#g")
 
     def test_init(self):
-        """
-        this method tests if our user object has been instantiated correctly
-        """
+        # this method tests if our user object has been instantiated correctly
         self.assertEqual(self.new_user.user_name, "James")
         self.assertEqual(self.new_user.password, "w3#g")
+
+    def test_save_user(self):
+        # test if the user object is being added to our user_list
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
 
 
 if __name__ == '__main__':
