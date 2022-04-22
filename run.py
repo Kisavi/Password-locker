76a8b6fa9904e0b42348783d19be49de54fb28f3
Kisavi <deniskisavi@gmail.com>
2022-04-23 01:41:18 +0300

    replace if statements with while loop to prevent the next code block from execution incase user puts in wrong info

--- run.py
+++ run.py
@@ -1,24 +1,30 @@
-from user import  User
+from user import User
+
 
 def main():
 
-    response = input(
-        """
-        Hello and welcome to password locker. Instructions to navigate. PRESS:
-        1 to Sign up
-        2 to Login to your account
-        3 to create custom passwords
-        4 to generate random passwords
-        5 to view saved credentials
-        6 to delete a credential 
-        """
-    )
+    print("Hello and welcome to password locker. Sign up by filling in the required fields.")
+    user_name = input("Username: ")
+    password = input("Password: ")
+    confirm_password = input("Confirm password: ")
 
-    if response == "1":
-        print("Fill in the required fields to Signup")
-        user_name = input("Username: ")
+    while password != confirm_password:
+        print("Your passwords did not match")
         password = input("Password: ")
         confirm_password = input("Confirm password: ")
 
+    else:
+        print(f"{user_name}, your account has been created successfully. Proceed to log in")
+        login_user_name = input("Enter your username: ")
+        login_password = input("Enter your password: ")
+
+    while login_password != password or login_user_name != user_name:
+        print("You have entered a wrong username or password")
+        login_user_name = input("Enter your username: ")
+        login_password = input("Enter your password: ")
+
+    else:
+        print(f"{user_name}, welcome to your account")
 
 
+main()
--- user-test.py
+++ user-test.py
@@ -26,5 +26,5 @@ class TestUser(unittest.TestCase):
         self.assertEqual(len(User.user_list), 1)
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
