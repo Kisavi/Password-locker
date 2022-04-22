52ae0f787918a90cafa9bcf1ffc4c4489520759e
Kisavi <deniskisavi@gmail.com>
2022-04-22 23:56:16 +0300

    create instructions to user on how to interact with the app and collect their sign up info

--- run.py
+++ run.py
@@ -1,1 +1,24 @@
+from user import  User
+
 def main():
+
+    response = input(
+        """
+        Hello and welcome to password locker. Instructions to navigate. PRESS:
+        1 to Sign up
+        2 to Login to your account
+        3 to create custom passwords
+        4 to generate random passwords
+        5 to view saved credentials
+        6 to delete a credential 
+        """
+    )
+
+    if response == "1":
+        print("Fill in the required fields to Signup")
+        user_name = input("Username: ")
+        password = input("Password: ")
+        confirm_password = input("Confirm password: ")
+
+
+
