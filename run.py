fc907ca5cae283ec1738d29c968b28a64e7b003c
Kisavi <deniskisavi@gmail.com>
2022-04-24 19:29:15 +0300

    add the shebang line

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from user import User
-from credentials import Credentials
+#!/usr/bin/env python3.8
 from user import User
 from credentials import Credentials
 from password_generator import PasswordGenerator
